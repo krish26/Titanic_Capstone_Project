f01a2907d12d65df9b315e3f1bfc033c8151ef37
krishna koumudi <koumudi26@gmail.com>
2026-02-05 11:41:51 +0100

    feature_engineering

---
+++ ML/feature_engineering.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def engineer_features(df: pd.DataFrame) -> pd.DataFrame:
+    # Age binning
+  
+    df['AgeGroup'] = pd.cut(
+        df['Age'], bins=[0,12,18,35,60,100],
+        labels=['Child','Teen','YoungAdult','Adult','Senior']
+    )
+
+    # Fare binning
+    df['Fare'] = df['Fare'].fillna(df['Fare'].median())
+    df['FareGroup'] = pd.qcut(df['Fare'], 4, labels=['Low','Mid','High','VeryHigh'])
+
+    # Family features
+    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+    df['IsAlone'] = (df['FamilySize']==1).astype(int)
+
+    return df
