2b7f92dd4182c2e3715902706727e44450f92e9c
JaneKorn <yevheniiakorn@gmail.com>
2026-02-06 16:56:31 +0100

    Add PredictionAdmin class

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Prediction
 
-# Register your models here.
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pclass', 'sex', 'age', 'fare', 'embarked', 
+                    'family_size', 'is_alone', 'survived', 'survival_probability', 'created_at')
+    list_filter = ('pclass', 'sex', 'embarked', 'survived', 'is_alone')
+    search_fields = ('id', 'sex', 'embarked')
+    readonly_fields = ('survived', 'survival_probability', 'created_at')
