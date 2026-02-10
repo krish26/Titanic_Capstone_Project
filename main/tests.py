6c3113b7e23f8e302fb8f01223d3c1fddb0a2065
Yuliya Hagberg <98893642+YuliyaHagberg@users.noreply.github.com>
2026-02-10 15:29:29 +0100

    Created tests for prediction form input validation

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
+from .forms import PredictionForm
 
-# Create your tests here.
+class PredictionFormTest(TestCase):
+
+    def test_valid_form(self):
+        form_data = {
+            "pclass": 1,
+            "sex": 0,
+            "age": 10,
+            "sibsp": 1,
+            "parch": 2,
+            "fare": 10,
+            "embarked": 3,
+        }
+        form = PredictionForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_age(self):
+        form_data = {
+            "pclass": 1,
+            "sex": 0,
+            "age": -5,
+            "sibsp": 0,
+            "parch": 0,
+            "fare": 10,
+            "embarked": 1,
+        }
+        form = PredictionForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("age", form.errors)
+
+    def test_invalid_sibsp(self):
+            form_data = {
+                "pclass": 1,
+                "sex": 0,
+                "age": 5,
+                "sibsp": -1,
+                "parch": 0,
+                "fare": 10,
+                "embarked": 1,
+            }
+            form = PredictionForm(data=form_data)
+            self.assertFalse(form.is_valid())
+            self.assertIn("sibsp", form.errors)
+
+    def test_invalid_parch(self):
+        form_data = {
+            "pclass": 1,
+            "sex": 0,
+            "age": -5,
+            "sibsp": 0,
+            "parch": -3,
+            "fare": 20,
+            "embarked": 1,
+        }
+        form = PredictionForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("parch", form.errors)
+
+    def test_invalid_fare(self):
+        form_data = {
+            "pclass": 1,
+            "sex": 0,
+            "age": -5,
+            "sibsp": 0,
+            "parch": 3,
+            "fare": -20,
+            "embarked": 1,
+        }
+        form = PredictionForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("fare", form.errors)
