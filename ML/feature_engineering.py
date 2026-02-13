e01d680cffc86f5d6ecd22a1967860c39aae5f7e
ApekshaShenoy <apekshashenoy.1992@gmail.com>
2026-02-13 09:24:26 +0100

    update

--- ML/feature_engineering.py
+++ ML/feature_engineering.py
@@ -1,41 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# def engineer_features(df: pd.DataFrame):
-    
-#     # Title extraction from Name
-#     df['Title'] = df['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
-
-#     # Standardize some titles
-#     df['Title'] = df['Title'].replace({
-#         'Mlle': 'Miss',
-#         'Ms': 'Miss',
-#         'Mme': 'Mrs'
-#     })
-
-#     # Replace rare titles with 'Rare'
-#     rare_titles = ["Lady","Countess","Capt","Col","Don","Dr","Major","Rev","Sir","Jonkheer","Dona"]
-#     df['Title'] = df['Title'].replace(rare_titles, "Rare")
-
-#     # Age binning to pdata_inspect notebook to manage missing values better and to maintain execution flow.
-#     #     # df['AgeGroup'] = pd.cut(
-#     #     df['Age'], bins=[0,12,18,35,60,100],
-#     #     labels=['Child','Teen','YoungAdult','Adult','Senior']
-#     # )
-
-#     # Fare binning
-    
-#     #4 is fare is split into different quartlies
-#     #<25% - low fares , 25%-50% - mid fares, 50%-75% - high fares, >75% - very high fares
-    
-#     df['FareGroup'] = pd.qcut(df['Fare'], 4, labels=['Low','Mid','High','VeryHigh'])
-
-#     # Family features
-#     df['FamilySize'] = df['SibSp'] + df['Parch'] + 1 #including self
-#     df['IsAlone'] = (df['FamilySize']==1).astype(int) #1 if alone, 0 otherwise
-
-#     return df
-
 def extract_title(df: pd.DataFrame):
     # Extract Title from Name
     df['Title'] = df['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
@@ -66,9 +31,8 @@ def create_age_bins(df: pd.DataFrame):
     # Create AgeGroup
     df['AgeGroup'] = pd.cut(
         df['Age'],
-        bins=[0,12,18,35,60,np.inf],
+        bins=[0,12,18,35,60,100],
         labels=['Child','Teen','YoungAdult','Adult','Senior'],
-        include_lowest=True
     )
 
     return df
