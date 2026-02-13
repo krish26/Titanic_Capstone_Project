319902bb2ead56ef3bc0743900d7cee69e7076f4
Yuliya Hagberg <98893642+YuliyaHagberg@users.noreply.github.com>
2026-02-13 10:27:58 +0100

    Revert "trying to get things back , as the previous changes affected accuracy"
    This reverts commit cf9c25e2f7885e608970e8e467d00aa078ceb977.

--- ML/feature_engineering.py
+++ ML/feature_engineering.py
@@ -1,14 +1,46 @@
 import pandas as pd
 import numpy as np
 
-def engineer_features(df: pd.DataFrame):
+def extract_title(df: pd.DataFrame):
+    # Extract Title from Name
+    df['Title'] = df['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
 
-    # Age binning
+    # Standardize equivalent titles
+    df['Title'] = df['Title'].replace({
+        'Mlle': 'Miss',
+        'Ms': 'Miss',
+        'Mme': 'Mrs'
+    })
+
+    # Group rare titles
+    rare_titles = ["Lady","Countess","Capt","Col","Don","Dr","Major","Rev","Sir","Jonkheer","Dona"]
+    df['Title'] = df['Title'].replace(rare_titles, "Rare")
+
+    return df
+
+
+def create_age_bins(df: pd.DataFrame):
+    # Fill missing Age using median per Title
+    df['Age'] = df.groupby('Title')['Age'].transform(
+        lambda x: x.fillna(x.median())
+    )
+
+    # Just in case any Title group had all NaN
+    df['Age'] = df['Age'].fillna(df['Age'].median())
+
+    # Create AgeGroup
     df['AgeGroup'] = pd.cut(
         df['Age'],
         bins=[0,12,18,35,60,100],
-        labels=['Child','Teen','YoungAdult','Adult','Senior']
+        labels=['Child','Teen','YoungAdult','Adult','Senior'],
     )
+
+    return df
+
+
+def engineer_features(df: pd.DataFrame):
+    df = extract_title(df)
+    df = create_age_bins(df)
 
     # Fare binning
     df['FareGroup'] = pd.qcut(
@@ -21,3 +53,5 @@ def engineer_features(df: pd.DataFrame):
     df['IsAlone'] = (df['FamilySize'] == 1).astype(int)
 
     return df
+
+
